bf8eaf3765ddb405123cc43e444260b05b030ceb
sistemas <sistemas@sistemas-dnl>
2011-12-02 15:16:11 -0300

    acomode los fastTask del login Signed-off-by:sistemas <sistemas@sistemas-dnl>

--- application/modules/centrales/models/astav_model.php
+++ application/modules/centrales/models/astav_model.php
@@ -4,10 +4,29 @@
  *
  * @author sistemas
  */
-class Astav_model extends Asterisk_model{
+class Astav_model extends CI_Model{
+  private $DB;
   function __construct() {
     parent::__construct();
-    $this->_setDB('asTAV');
+    $this->DB = $this->load->database('asTAV', true);
+  }
+  function getInternos(){
+    $this->DB->select('extension');
+    $this->DB->select('name');
+    $this->DB->select('sipname');
+    $this->DB->from('users');
+    $this->DB->order_by('extension');
+    $q = $this->DB->get();
+    return $q->result();
+  }
+  function getLineas(){
+    $this->DB->select('trunkid');
+    $this->DB->select('name');
+    $this->DB->select('tech');
+    $this->DB->from('trunks');
+    $this->DB->order_by('trunkid');
+    $q = $this->DB->get();
+    return $q->result();
   }
 }
 
